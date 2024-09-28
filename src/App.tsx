158d4c9ec7843ee304fecb5d02834f8a09ef66e7
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2024-09-28 18:38:41 -0500

    Реализовал подсчет элементов в меню с выводом общей суммы тотал.

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,14 @@ interface Menu {
     id: string;
     name: string;
     price: number;
-    image: string
+    image: string;
+}
+
+interface OrderItem {
+    id: string;
+    name: string;
+    price: number;
+    count: number;
 }
 
 const Menu: Menu[] = [
@@ -22,39 +29,57 @@ const Menu: Menu[] = [
     {id: '3', name: 'Fries', price: 45, image: fries},
     {id: '4', name: 'Coffee', price: 70, image: coffee},
     {id: '5', name: 'Tea', price: 50, image: tea},
-    {id: '6', name: 'Cola', price: 40, image: cola}
+    {id: '6', name: 'Cola', price: 40, image: cola},
 ];
 
 const App = () => {
-    const [orders, setOrders] = useState<Menu[]>([]);
+    const [orders, setOrders] = useState<OrderItem[]>([]);
 
     const getItemInfo = (name: string) => {
         const selectedItem = Menu.find(item => item.name === name);
 
         if (selectedItem) {
-            setOrders(prevState => [...prevState, selectedItem]);
+            setOrders(prevState => {
+                const existingItem = prevState.find(item => item.name === name);
+
+                if (existingItem) {
+                    return prevState.map(item => item.name === name ? {...item, count: item.count + 1} : item);
+                } else {
+                    return [...prevState, {...selectedItem, count: 1}];
+                }
+            });
         }
     };
 
+    const getTotal = () => {
+        return orders.reduce((acc, number) => {
+            return acc + (number.price * number.count);
+        }, 0);
+    };
+
     const itemButton = Menu.map(i => (
-            <Items key={i.id} price={i.price} name={i.name} image={i.image} onAddButton={() => getItemInfo(i.name)}/>
+        <Items key={i.id} price={i.price} name={i.name} image={i.image} onAddButton={() => getItemInfo(i.name)}/>
+    ));
+
+    const orderedItems = orders.map(item => (
+        <OrderDetails key={item.id}
+                      name={item.name}
+                      count={item.count}
+                      price={item.price}
+        />
     ));
 
     return (
         <div className='appStyle'>
             <div className='mainMenu'>
                 <div className='orderedItems'>
-                    <h2 className='textInfo'>Order Details:</h2>
-                    {orders.length === 0 ? (
-                        <p>No items in your order.</p>
-                    ) : (
-                        orders.map(item => (
-                            <OrderDetails key={item.id} name={item.name} />
-                        ))
-                    )}
+                    <h2 className='textInfo'>Order:</h2>
+                    {orders.length > 0 ? <h4>Total price: {getTotal()} KGS</h4> :
+                        <p className='ordered-text'>You are welcome to make an order from the Menu!</p>}
+                    {orderedItems}
                 </div>
                 <div className='menuItems'>
-                    <h2 className='menu-title'>Add Items: </h2>
+                    <h2 className='menu-title'>Menu: </h2>
                     <div className='gridContainer'>
                         {itemButton}
                     </div>
--- src/components/OrderDetails/OrderDetails.tsx
+++ src/components/OrderDetails/OrderDetails.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 interface Props {
-    name: any;
+    name: string;
+    count: number;
+    price: number;
 }
 
-const OrderDetails: React.FC<Props> = ({name}) => {
+const OrderDetails: React.FC<Props> = ({name, count, price}) => {
     return (
         <div className='orderDetailsMenu'>
-            <p>
-                <span>{name}</span>
+            <p className='orderItems'>
+                <span>{name}</span> <span>x{count}</span> <strong>{price} KGS</strong>
             </p>
         </div>
     );
